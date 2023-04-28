e6f34b82f7d01a65471da03498f63956840d5dfe
Higor <higorpiresoliveira@gmail.com>
2023-04-28 13:43:08 -0300

    uses puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const hello = 'Hello World';
+import puppeteer from 'puppeteer';
 
-console.log(hello);
+async function main(): Promise<void> {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://google.com');
+
+  await page.screenshot({ path: 'example.png' });
+
+  await page.waitForTimeout(4000);
+
+  await browser.close();
+}
+
+await main();
