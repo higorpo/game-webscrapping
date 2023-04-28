329bb7c87fa6c7e328797101762e6c61e82194b6
Higor <higorpiresoliveira@gmail.com>
2023-04-28 14:42:23 -0300

    creates base scrapper

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { type Page } from 'puppeteer';
 
 async function main(): Promise<void> {
   const browser = await puppeteer.launch({
@@ -7,13 +7,65 @@ async function main(): Promise<void> {
 
   const page = await browser.newPage();
 
-  await page.goto('https://google.com');
+  await page.goto('https://slotcatalog.com/pt/The-Best-Slots?typ=2');
 
-  await page.screenshot({ path: 'example.png' });
+  let currentPage = 1;
+  const maxPage = 35;
+  const allElements: Array<{ href: string | undefined }> = [];
 
-  await page.waitForTimeout(4000);
+  while (currentPage < maxPage) {
+    await awaitToCollectData(page);
+
+    await page.evaluate(() => window.scrollTo({ top: 300 }));
+
+    console.log('buscando dados!');
+
+    await page.screenshot({ path: `tmp/selecting-${currentPage}.png` });
+
+    const elements = await page.$$eval(
+      '.providerCardOut > .providerCard',
+      (elements) =>
+        elements.map((element) => ({
+          href:
+            element.querySelector('a[href]')?.getAttribute('href') ?? undefined,
+        })),
+    );
+
+    allElements.push(...elements);
+
+    currentPage += 1;
+
+    await page.waitForSelector(
+      '.navpag > .navpag-center > .navpag-cont > ul > li:not(.active) > a',
+    );
+
+    await page.evaluate((currentPage) => {
+      const button = Array.from(
+        document.querySelectorAll(
+          '.navpag > .navpag-center > .navpag-cont > ul > li:not(.active) > a',
+        ),
+      ).find((element) => element.innerHTML === `${currentPage}`) as
+        | HTMLElement
+        | undefined;
+
+      button?.click();
+    }, currentPage);
+  }
 
   await browser.close();
+}
+
+async function awaitToCollectData(page: Page): Promise<void> {
+  try {
+    console.log('aguardando');
+    await page.waitForSelector('.providerCardOut > .providerCard', {
+      timeout: 5000,
+    });
+  } catch (e) {
+    console.log('deu erro');
+    await page.reload();
+    await awaitToCollectData(page);
+  }
 }
 
 await main();
